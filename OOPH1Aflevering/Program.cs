180c6f94ffd0d251906af5a79bfcab668babe0b7
Kristian Pedersen <Icfafn@gmail.com>
2022-02-28 09:10:08 +0100

    Add project files.

---
+++ OOPH1Aflevering/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class MotorKøretøj
+{ 
+    public int HesteKræfter { get; set; } 
+}
+
+
+internal class Bil : MotorKøretøj
+{ 
+    public string Mærke { get; set; } 
+}
+
+
+internal class Audi : Bil
+{ 
+    public string Model { get; set; } 
+}
+
+MotorKøretøj motorKøretøj = new MotorKøretøj();
+
+MotorKøretøj motorKøretøj = new Bil();
+
+MotorKøretøj motorKøretøj = new Audi();
+
+Audi audi = new Audi();
+// Dette er ikke en fejl... (Kommenter hvorfor!!!)
+Audi audi = new Bil();
+
+Audi audi = new MotorKøretøj();
+
+//2.Hvis man istantiere Audi klassen.
+//Kan man igennem audi klassens instans se Mærke og HesteKræfterproperty’erne?
+
+//3.Hvis man istantiere MotorKøretøj klassen.
+//Kan man igennem motorkøretøj instans se Mærke og Model property’erne?
